3b0178e74b16c192ce6c0bd2246132c24e1dec35
BM <hotwhitewind@gmail.com>
2020-11-17 14:25:58 +0300

    Add project

---
+++ ConvertGeoNamesDBToMongoDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConvertGeoNamesDBToMongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
